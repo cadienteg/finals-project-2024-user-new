dc22fc4323a34c45add63b4f7f67bf475578f220
cadienteg <160442130+cadienteg@users.noreply.github.com>
2024-05-12 11:26:34 +0800

    Update MainCreate

--- src/DTO/Main.java
+++ src/DTO/Main.java
@@ -1,9 +1,21 @@
-package DTO;
+package com.ctu.reservationportal.infrastructure;
 
+import com.ctu.reservationportal.dbservices.InsertRecord;
+import com.ctu.reservationportal.model.UserInfo;
+
+import java.sql.SQLException;
 import java.util.Scanner;
 
-public class Main {
-
+//Main class for user registration
+public class MainCreate {
+    /**
+     * Prompts the user for input and validates it using a provided validator.
+     *
+     * @param scanner       Scanner object for input
+     * @param promptMessage Message to display when prompting for input
+     * @param validator     Validator for input validation
+     * @return Validated user input
+     */
     public static String promptForInput(Scanner scanner, String promptMessage, Validator validator) {
         String input;
         boolean isValid;
@@ -12,24 +24,33 @@ public class Main {
             input = scanner.nextLine();
             isValid = validator.isValid(input);
             if (!isValid) {
-                System.out.println("Please input correctly.");
+                System.out.println("Oops! Looks like there's a typo in there! " +
+                        "Please input details correctly.");
             }
         } while (!isValid);
         return input;
     }
 
+    //Interface for input validation.
     interface Validator {
         boolean isValid(String input);
     }
 
-    public static void main(String[] args) {
+    //Main method for user registration.
+    public static void main(String[] args) throws SQLException {
+        // Initialization of Scanner object to read user input
         Scanner scanner = new Scanner(System.in);
-
+        // Create a new UserInfo object to store user information
         UserInfo userInfo = new UserInfo();
-        System.out.println("\n     WELCOME TO USER REGISTRATION!");
-        System.out.println("PLEASE ENTER THE RIGHT DETAILS CORRECTLY\n");
+        // Print welcome message and instructions for user registration
+        System.out.println("\n              WELCOME TO USER REGISTRATION!");
+        System.out.println("Each field has specific guidelines.To ensure a smooth process.\n" +
+                "Please review the information and enter details that match those guidelines\n");
 
-        // Prompt for user details
+
+        // Prompt for and validate user details
+        // Each detail is prompted for using the promptForInput method with appropriate validators
+        //In the promptForInput method, Validators::isValidName is passed as an argument to indicate that the input provided by the user should be validated using the isValidName method defined in the Validators class.
         String firstName = promptForInput(scanner, "Enter First name: ", Validators::isValidName);
         userInfo.setFirstName(firstName);
 
@@ -61,32 +82,74 @@ public class Main {
         userInfo.setCity(city);
 
         int zipcode = Integer.parseInt(promptForInput(scanner, "Enter ZIP Code: ", Validators::isValidZIPCode));
-        userInfo.setZIPcode(String.valueOf(zipcode));
+        userInfo.setZIPcode(zipcode);
 
         String nationality = promptForInput(scanner, "Enter Nationality: ", Validators::isValidName);
         userInfo.setNationality(nationality);
 
-        String gender = promptForInput(scanner, "Enter Gender (M-Male/F-Female/N-Not to say): ", Validators::isValidGender);
+        String gender = promptForInput(scanner, "Enter Gender (M-Male|F-Female|N-Not to say): ", Validators::isValidGender);
         userInfo.setGender(gender);
 
+        InsertRecord insertRecord = new InsertRecord();
+        // Insert the user record into the database
+        insertRecord.insertUserRecord(userInfo);
+
+        // Display the details provided by the user during the registration process.
+        System.out.println("\nDETAILS PROVIDED");
+        System.out.println("\nUSER DETAILS");
+        System.out.println("First name: " + userInfo.getFirstName());
+        System.out.println("Middle name: " + userInfo.getMiddleName());
+        System.out.println("Last name: " + userInfo.getLastName());
+        System.out.println("Birthdate: " + userInfo.getBirthdate());
+        System.out.println("Email: " + userInfo.getEmail());
+        System.out.println("Phone Number: " + userInfo.getPhoneNumber());
+        System.out.println("\nHOME ADDRESS DETAILS");
+        System.out.println("Street: " + userInfo.getStreet());
+        System.out.println("Barangay: " + userInfo.getBarangay());
+        System.out.println("Municipality: " + userInfo.getMunicipality());
+        System.out.println("City: " + userInfo.getCity());
+        System.out.println("ZIP Code: " + userInfo.getZIPcode());
+
+        System.out.println("\nUSER OTHER DETAILS");
+        System.out.println("Nationality: " + userInfo.getNationality());
+        System.out.println("Gender: " + userInfo.getGender());
+
+
+
         // Validation of role at school
-        System.out.print("\nEnter role at school (Teacher/Student/Staff): ");
-        String roleAtSchool = scanner.nextLine();
-        userInfo.setroleAtSchool(roleAtSchool);
+        // Check the role at school and validate it
+        String roleAtSchool;
+        do {
+            System.out.print("\nEnter role at school (Teacher/Student/Staff): ");
+            roleAtSchool = scanner.nextLine();
+            userInfo.setroleAtSchool(roleAtSchool);
 
+            if (!roleAtSchool.equalsIgnoreCase("Teacher") &&
+                    !roleAtSchool.equalsIgnoreCase("Student") &&
+                    !roleAtSchool.equalsIgnoreCase("Staff")) {
+                System.out.println("Invalid role. Please enter Teacher, Student, or Staff.");
+            }
+        } while (!roleAtSchool.equalsIgnoreCase("Teacher") &&
+                !roleAtSchool.equalsIgnoreCase("Student") &&
+                !roleAtSchool.equalsIgnoreCase("Staff"));
+        // Check if the user is a teacher
         if (roleAtSchool.equalsIgnoreCase("Teacher")) {
-            // Redirect to teaching details class
+            System.out.println("\nWelcome to Teaching Details.");
             TeachingDetails teachingDetails = new TeachingDetails();
-            teachingDetails.inputSubjectNames();
-            // Generate registration code and verify
-        } else if (roleAtSchool.equalsIgnoreCase("Student") || roleAtSchool.equalsIgnoreCase("Staff")) {
-            System.out.println("\nSorry, only teachers are allowed in this portal.");
-            System.out.println("\nProceeding to checking details...");
-            // Redirect to check details class
+            teachingDetails.CollectTeachingDetails(scanner);
+        } else {
+            // Call Check user detail
             CheckDetails.checkDetails(userInfo);
+            // Call Edit user info
+            UserInfoEditor editUserInfo = new UserInfoEditor(userInfo);
+            editUserInfo.editUserInfo();
+            //Call create account
+            CreateAccount.createAccount(scanner);
+            //Call preferRole
+            PreferRole.selectPreferRole(userInfo);
+            // End the program after all actions are completed
+            System.out.println("You have succesfully registered.");
+            System.exit(0);
         }
-
-        scanner.close();
     }
-
 }
