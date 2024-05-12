dcc19525cfcbbed16f00de9e61c0b9804ffe7bc3
cadienteg <160442130+cadienteg@users.noreply.github.com>
2024-05-12 13:52:34 +0800

    Update CheckDetails

--- src/DTO/CheckDetails.java
+++ src/DTO/CheckDetails.java
@@ -1,7 +1,8 @@
-package DTO;
+package com.ctu.reservationportal.infrastructure;
+
+import com.ctu.reservationportal.model.UserInfo;
 
 import java.util.Scanner;
-
 public class CheckDetails {
 
     /*
@@ -9,36 +10,57 @@ public class CheckDetails {
      */
     public static void checkDetails(UserInfo userInfo) {
         Scanner scanner = new Scanner(System.in);
+        boolean detailsConfirmed = false;
 
-        System.out.println("** Checking Details **");
-        System.out.println("Name: " + userInfo.getFirstName() + " " + userInfo.getMiddleName() + " " + userInfo.getLastName());
-        System.out.println("Birthdate: " + userInfo.getBirthdate());
-        System.out.println("Email: " + userInfo.getEmail());
-        System.out.println("HOME ADDRESS DETAILS");
-        System.out.println("Home Address: " + userInfo.getStreet() + ", " + userInfo.getBarangay() + ", "
-                + userInfo.getMunicipality() + ", " + userInfo.getCity());
-        System.out.println("ZIP code: " + userInfo.getZIPcode());
-        System.out.println("");
-        System.out.println("USER OTHER DETAILS");
-        System.out.println("Nationality: " + userInfo.getNationality());
-        System.out.println("Gender: " + userInfo.getGender());
+        do {
+            // Displaying the user details for confirmation
+            System.out.println("** Sorry you are not allowed in this portal**");
+            System.out.println("** Checking Details **");
+            System.out.println("Name: " + userInfo.getFirstName() + " " + userInfo.getMiddleName() + " " + userInfo.getLastName());
+            System.out.println("Birthdate: " + userInfo.getBirthdate());
+            System.out.println("Email: " + userInfo.getEmail());
+            System.out.println("HOME ADDRESS DETAILS");
+            System.out.println("Home Address: " + userInfo.getStreet() + ", " + userInfo.getBarangay() + ", "
+                    + userInfo.getMunicipality() + ", " + userInfo.getCity());
+            System.out.println("ZIP code: " + userInfo.getZIPcode());
+            System.out.println("");
+            System.out.println("USER OTHER DETAILS");
+            System.out.println("Nationality: " + userInfo.getNationality());
+            System.out.println("Gender: " + userInfo.getGender());
 
-        // Prompt user for confirmation after checking
-        System.out.println("Are the details correct?");
-        System.out.println("1: Edit Details");
-        System.out.println("2: Skip Checking");
-        System.out.println("Please enter your choice: ");
+            // Prompt user for confirmation after checking
+            System.out.println("Are the details correct?");
+            System.out.println("1: Edit Details");
+            System.out.println("2: Proceed to Registration Code");
+            System.out.println("Please enter your choice: ");
 
-        int choice = scanner.nextInt();
-        scanner.nextLine(); // Consume newline character
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline character
 
-        if (choice == 2) {
-            System.out.println("Details confirmed. Proceeding to Registration Code .");
-            RegistrationCode registrationCode = new RegistrationCode();
-            RegistrationCode.displayRegistrationCode();
-        } else if (choice == 1) {
-            // Call UserInfoEditor to edit user information (optional)
-            UserInfoEditor userInfoEditor = new UserInfoEditor(userInfo);  // Pass userInfo object (optional)
-            userInfoEditor.editUserInfo();
-        }
-    }}
+            if (choice == 2) {
+                // Proceed to generate registration code
+                System.out.println("Details confirmed. Proceeding to Registration Code.");
+                RegistrationCode generatedCode = new RegistrationCode();
+                generatedCode.displayRegistrationCode();
+                detailsConfirmed = true;
+
+                // Proceed to create account
+                CreateAccount.createAccount(scanner);
+
+                // Redirect to the PreferRole clasS
+
+                // Call PreferRole.selectPreferRole() with the userInfo object
+                PreferRole.selectPreferRole(userInfo);
+
+                // Break out of the loop after displaying the registration code
+                break;
+            } else if (choice == 1) {
+                // Edit user information if choice is 1
+                UserInfoEditor editor = new UserInfoEditor(userInfo);
+                editor.editUserInfo();
+            } else {
+                System.out.println("Invalid choice.");
+            }
+        } while (!detailsConfirmed); // Repeat the process until details are confirmed
+    }
+}
